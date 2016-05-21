8b3b7412fc502ee3ac3470cf503b4896189d0b72
Robert Black <rbla4655@gmail.com>
2016-05-20 20:19:33 -0400

    heroku test

--- app/model/setup-passport.js
+++ app/model/setup-passport.js
@@ -2,10 +2,10 @@ var passport = require('passport');
 var Auth0Strategy = require('passport-auth0');
 
 var strategy = new Auth0Strategy({
-    domain: 'bryantbello.auth0.com',
-    clientID: 'rIZ3xtbY00Y3XcGTAyykN7eROiNPlWEN',
-    clientSecret: 'esvFrsBW3b3HAlh9ruKUVnhfDY3BVsIsE-vY5nZXwRo1wA_FlWv3H1Wvv_DBdGxf',
-    callbackURL: '/public/restaurant.html'
+    domain:       'rbla3066.auth0.com',
+    clientID:     'dCLsz1hEmVYS3KFLS9pUT2LeVFitNRhO',
+    clientSecret: '7aYE3BvRnRHqjq9SYhA6BiVaHAnV5rr4FPVcKxpVajMpky5IBq7X0PmOHsQo8BBY',
+    callbackURL:  '/'
 }, function(accessToken, refreshToken, extraParams, profile, done) {
     // accessToken is the token to call Auth0 API (not needed in the most cases)
     // extraParams.id_token has the JSON Web Token
